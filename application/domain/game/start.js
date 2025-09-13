a9f35a6b2515d16a09799bda1df8e87c03488cbe
VanesStalkers <ivan.mironov@bk.ru>
2025-09-13 15:22:25 +0400

    (core) added domain.game.configs.devMode && domain.game.configs.tutorialImgPrefix

--- application/domain/game/start.js
+++ application/domain/game/start.js
@@ -1,4 +1,7 @@
 async () => {
+  domain.game.configs.devMode = process.env.NODE_ENV === 'development';
+  domain.game.configs.tutorialImgPrefix = domain.game.configs.devMode ? '' : '/TO_CHANGE';
+
   {
     const files = await node.fsp.readdir('./application/static/img/cards', { withFileTypes: true });
     const cardTemplates = Object.values(files).map((_) => _.name);
@@ -39,9 +42,9 @@ async () => {
             title: 'TO_CHANGE',
             icon: ['fas', 'microchip'],
             active: true,
-            url: process.env.NODE_ENV === 'development' ? 'http://localhost:TO_CHANGE' : url, // такой же как в package.json serve
+            url: domain.game.configs.devMode ? 'http://localhost:TO_CHANGE' : url, // такой же как в package.json serve
             serverUrl:
-              process.env.NODE_ENV === 'development' ? `http://localhost:${config.server.balancer}` : `${url}/api`,
+              domain.game.configs.devMode ? `http://localhost:${config.server.balancer}` : `${url}/api`,
             games,
           });
           return;
